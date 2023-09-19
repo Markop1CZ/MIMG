04127823dca9f43fc8ea9269d646865a3d960148
Markop1CZ <markop1@email.cz>
2023-09-19 21:32:23 +0200

    Work - subsampling: YCoCg, interpolation

--- compression.py
+++ compression.py
@@ -4,12 +4,18 @@ from PIL import Image
 
 ## Using YCoCg
 
+def sample_array(arr, ratio):
+    return numpy.interp(numpy.arange(0, len(arr), len(arr)/ratio), numpy.arange(0, len(arr)), arr)
+
+def subsample(yuv, vertical=2, horizontal=4):
+    pass
+    
 class ImageCompression:
     def __init__(self, pil_image):
         self.pil_image = pil_image
 
     def compress(self):
-        img_data = numpy.array(self.pil_img)
+        img_data = numpy.array(self.pil_image)
         ## convert to YCoCg
         m = [[1/4, 1/2, 1/4],
              [1/2, 0, -1/2],
@@ -17,6 +23,14 @@ class ImageCompression:
         
         yuv = numpy.dot(numpy.divide(img_data, 255), m)
 
+
+        ## subsample
+
+        return yuv
+
 def test():
-    i = ImageCompression()
+    pil_img = Image.open("test-image.bmp")
+    img = ImageCompression(pil_img)
+    img.compress()
     
+test()
