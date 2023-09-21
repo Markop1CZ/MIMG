eca7348a02a3ece85ca5649b1bbc0d158e085d97
Markop1CZ <markop1@email.cz>
2023-09-21 20:20:18 +0200

    First working compression

--- compression.py
+++ compression.py
@@ -1,5 +1,8 @@
 import numpy
 import math
+import struct
+import scipy
+import zlib
 from PIL import Image
 
 ## Using YCoCg
@@ -17,8 +20,10 @@ def ycocg_rgb(pixels):
 
     return numpy.rint(numpy.multiply(numpy.dot(pixels, m), 255)).astype(numpy.uint8)
 
-def subsample(pixels, vertical=2, horizontal=4):
-    pass
+def subsample(pixels, vertical=0.5, horizontal=0.25):
+    output = scipy.ndimage.zoom(pixels, (horizontal, vertical))
+
+    return output
     
 class ImageCompression:
     def __init__(self, pil_image):
@@ -28,12 +33,58 @@ class ImageCompression:
         return numpy.array(self.pil_image)
 
     def compress(self):
-        yuv = rgb_ycocg(self.get_pixels())  
+        buffer = bytearray()
 
-        return yuv
+        print(self.pil_image.size)
 
-    def decompress(self, yuv):
-        pixels = ycocg_rgb(yuv)
+        buffer += struct.pack("II", *self.pil_image.size)
+        
+        yuv = rgb_ycocg(self.get_pixels())
+
+        y,u,v = numpy.dsplit(yuv, 3)
+        y = y.reshape(y.shape[0], y.shape[1])
+        u = subsample(u[:,:,0])
+        v = subsample(v[:,:,0])
+
+        print(y, u, v)
+        print(y.shape, u.shape, v.shape)
+
+        for channel in y,u,v:
+            data = zlib.compress(channel.astype(numpy.single).tobytes())
+            length = len(data)
+
+            buffer += struct.pack("III", channel.shape[0], channel.shape[1], length)
+            buffer += data
+
+        return buffer
+
+    def decompress(self, buffer):
+        img_h,img_w = struct.unpack("II", buffer[0:8])
+        del buffer[0:8]
+
+        channels = []
+        
+        for i in range(3):
+            w,h,length = struct.unpack("III", buffer[0:12])
+            del buffer[0:12]
+
+            data = buffer[0:length]
+            del buffer[0:length]
+
+            channels.append(numpy.frombuffer(zlib.decompress(data), numpy.single).reshape(w,h))
+
+        y,u,v = channels
+        print(y,u,v)
+        print(y.shape, u.shape, v.shape)
+        u = subsample(u, img_h/h, img_w/w)
+        v = subsample(v, img_h/h, img_w/w)
+
+        pixels = numpy.empty((img_w, img_h, 3), dtype=numpy.single)
+        pixels[:,:,0] = y
+        pixels[:,:,1] = u
+        pixels[:,:,2] = v
+
+        pixels = ycocg_rgb(pixels)
 
         return pixels
 
@@ -45,6 +96,11 @@ def test():
     pix2 = ycocg_rgb(rgb_ycocg(pix1))
 
     assert pix1.all() == pix2.all(), "yuv conversion fail!"
+
+    compressed = img.compress()
+    result = img.decompress(compressed)
+    im = Image.fromarray(result)
+    im.show()
     
 if __name__ == "__main__": 
     test()
