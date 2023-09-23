54bed70c9306e13df5e61d22eb33fe71d0eca34c
Markop1CZ <markop1@email.cz>
2023-09-23 17:00:45 +0200

    Fix matrices, debug output color and Y,Co,Cg channels

--- compression.py
+++ compression.py
@@ -7,17 +7,17 @@ from PIL import Image
 
 ## Using YCoCg
 def rgb_ycocg(pixels):
-    m = [[1/4, 1/2, 1/4],
-         [1/2, 0, -1/2],
-         [-1/4, 1/2, -1/4]]
+    m = [[1/4, 1/2, -1/4],
+         [1/2, 0, 1/2],
+         [1/4, -1/2, -1/4]]
 
     return numpy.dot(numpy.divide(pixels, 255), m)
 
 def ycocg_rgb(pixels):
-    m = [[1, 1, -1],
-         [1, 0, 1],
-         [1, -1, -1]]
-
+    m = [[1, 1, 1],
+         [1, 0, -1],
+         [-1, 1, -1]]
+    
     return numpy.rint(numpy.multiply(numpy.dot(pixels, m), 255)).astype(numpy.uint8)
 
 def subsample(pixels, vertical=0.25, horizontal=0.5):
@@ -27,17 +27,19 @@ def subsample(pixels, vertical=0.25, horizontal=0.5):
     
 class ImageCompression:
     def __init__(self, pil_image):
-        self.pil_image = pil_image
+        self.pil_image = pil_image.convert("RGB")
 
     def get_pixels(self):
         return numpy.array(self.pil_image)
 
     def compress(self):
+        print("Compressing image...")
         buffer = bytearray()
+        img_w,img_h = self.pil_image.size
+    
+        print("w={0} h={1}".format(img_w, img_h))
 
-        print(self.pil_image.size)
-
-        buffer += struct.pack("II", *self.pil_image.size)
+        buffer += struct.pack("II", img_w, img_h)
         
         yuv = rgb_ycocg(self.get_pixels())
 
@@ -46,8 +48,7 @@ class ImageCompression:
         u = subsample(u[:,:,0])
         v = subsample(v[:,:,0])
 
-        print(y, u, v)
-        print(y.shape, u.shape, v.shape)
+        print("u={1} v={1}".format(y.shape, u.shape, v.shape))
 
         for channel in y,u,v:
             data = zlib.compress(channel.astype(numpy.single).tobytes())
@@ -56,9 +57,10 @@ class ImageCompression:
             buffer += struct.pack("III", channel.shape[0], channel.shape[1], length)
             buffer += data
 
-        return buffer
+        return buffer, (y, u, v)
 
     def decompress(self, buffer):
+        print("Decompressing image...")
         img_h,img_w = struct.unpack("II", buffer[0:8])
         del buffer[0:8]
 
@@ -74,8 +76,6 @@ class ImageCompression:
             channels.append(numpy.frombuffer(zlib.decompress(data), numpy.single).reshape(w,h))
 
         y,u,v = channels
-        print(y,u,v)
-        print(y.shape, u.shape, v.shape)
         u = subsample(u, img_h/h, img_w/w)
         v = subsample(v, img_h/h, img_w/w)
 
@@ -93,21 +93,51 @@ def test():
     import os
     
     images_folder = "test-images"
+    output_folder = "test-output"
+    if not os.path.exists(output_folder):
+        os.mkdir(output_folder)
+        
     for file in os.listdir(images_folder):
+        print("Converting {0}:".format(file))
+        img_name = os.path.splitext(file)[0]
+        
         pil_img = Image.open(os.path.join(images_folder, file))
         img = ImageCompression(pil_img)
 
-        compressed = img.compress()
+        compressed, debug_channels = img.compress()
+        for i,c in enumerate(list("yuv")):
+            debug_channel = debug_channels[i]
+            channel_data = numpy.empty((*debug_channel.shape, 3), dtype=numpy.single)
+            channel_data[:,:,i] = debug_channel
+            print(c, numpy.min(debug_channel), numpy.max(debug_channel))
+            channel_pixels = ycocg_rgb(channel_data)
+            channel_img = Image.fromarray(channel_pixels, mode="RGB")
+            channel_img.save(os.path.join(output_folder, "{0}-{1}.png".format(img_name, c)))
+        
         compressed_len = len(compressed)
         result_img = Image.fromarray(img.decompress(compressed))
 
+        ## compare against PNG
         b = BytesIO()
-        pil_img.save(b, format="jpeg")
-        jpeg_len = b.getbuffer().nbytes
+        pil_img.save(b, format="png")
+        png_len = b.getbuffer().nbytes
 
-        print("{0} - {1}b {2}b = {3:.2f}%".format(file, compressed_len, jpeg_len, (compressed_len/jpeg_len)*100))
+        print("-> {0} compressed={1:.2f}Mb png={2:.2f}Mb ratio={3:.2f}%".format(file, compressed_len/1048576, png_len/1048576, (compressed_len/png_len)*100))
 
-        result_img.show()
+        result_img.save(os.path.join(output_folder, img_name+"-color.png"))
+
+## should output a single color red image
+def ycocg_test():
+    import os
+    w,h = 256, 256
+    channel_data = numpy.full((w, h, 3), 0, dtype=numpy.single)
+    channel_data[:,:,0] = numpy.full((w, h), 1/4, dtype=numpy.single)
+    channel_data[:,:,1] = numpy.full((w, h), 1/2, dtype=numpy.single)
+    channel_data[:,:,2] = numpy.full((w, h), -1/4, dtype=numpy.single)
+    channel_pixels = ycocg_rgb(channel_data)
+    channel_img = Image.fromarray(channel_pixels, mode="RGB")
+    channel_img.save("ycocg-test.png")
           
 if __name__ == "__main__": 
     test()
+    ycocg_test()
