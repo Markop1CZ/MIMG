e1a9f8b1533884e254d805bc683e4b87f33b64e6
Markop1CZ <markop1@email.cz>
2023-09-29 13:48:31 +0200

    Add read write functions for dct of tiles

--- dct.py
+++ dct.py
@@ -3,11 +3,12 @@ from scipy.fftpack import dct, idct
 import numpy
 import math
 import zlib
+import struct
 
 def img_to_tiles(img, size):
     pixels = numpy.array(img)
     
-    tile_w,tile_h = size,size
+    tile_w,tile_h = size
     img_w,img_h = img.size
     last_w, last_h = img_w%tile_w, img_h%tile_h
 
@@ -30,7 +31,7 @@ def tile_to_bytes(img_tile):
     pixels = numpy.add(numpy.around(pixels, 1), 0.0)
     ##print(arr)
     
-    compressed = zlib.compress(pixels.astype(numpy.half))
+    compressed = zlib.compress(pixels.astype(numpy.half).tobytes())
     buf += bytes([*pixels.shape, len(compressed)]) + compressed
     
     return buf
@@ -52,30 +53,44 @@ def read_tiles(data):
 
     return tiles
 
+## read/write functions ("api" use)
+def write_image_tiles(im, tile_size):
+    im = im.convert("L")
+
+    tiles = img_to_tiles(im, tile_size)
+    
+    buf = struct.pack("IIbb", *im.size, *tile_size)
+    for t in tiles:
+        buf += tile_to_bytes(t)
+
+    return buf
+
+def read_image_tiles(buf):
+    im_w,im_h,tile_w,tile_h = struct.unpack("IIbb", buf[:10])
+
+    decompressed_tiles = read_tiles(buf[10:])
+    output_image = Image.new("L", (im_w,im_h))
+    num = 0
+    
+    for j in range(math.ceil(im_h/tile_h)):
+        for i in range(math.ceil(im_w/tile_w)):
+            tile = Image.fromarray(decompressed_tiles[num], "L")
+            output_image.paste(tile, (i*tile_w, j*tile_h))
+            
+            num += 1
+
+    return output_image
+
+## test
 def test():
     tile_size = 16
     im = Image.open("dct-test.png").convert("L")
     w,h = im.size
     
-    tiles = img_to_tiles(im, tile_size)
-
-    buf = b""
-    for t in tiles:
-        buf += tile_to_bytes(t)
-
+    buf = write_image_tiles(im, (tile_size, tile_size))
     print(len(buf))
 
-    decompressed_tiles = read_tiles(buf)
-    output_image = Image.new("L", (w,h))
-    num = 0
-    
-    for j in range(math.ceil(h/tile_size)):
-        for i in range(math.ceil(w/tile_size)):
-            tile = Image.fromarray(decompressed_tiles[num], "L")
-            output_image.paste(tile, (i*tile_size, j*tile_size))
-            
-            num += 1
-
+    output_image = read_image_tiles(buf)
     output_image.save("dct-test-out.png")
     
 if __name__ == "__main__":
