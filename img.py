e83f749a40ead56177b2e83a5e65d68c14bce439
Markop1CZ <markop1@email.cz>
2024-04-28 11:59:53 +0200

    Complete rewrite, add RLE, zigzag, ZLIB.

--- img.py
+++ img.py
@@ -9,7 +9,6 @@ from scipy.fftpack import dct, idct
 from io import BytesIO
 
 FILE_SIG = b"MIMG"
-FILE_SIG_L = 4
 
 DCT_QUANT_DEFAULT = \
     numpy.array([[16,  11,  10,  16,  24,  40,  51,  61],
@@ -21,15 +20,17 @@ DCT_QUANT_DEFAULT = \
                  [49,  64,  78,  87, 103, 121, 120, 101],
                  [72,  92,  95,  98, 112, 100, 103,  99]])
 
-DCT_QUANT_LOW = \
-    numpy.array([[2,  9,  11, 12, 14, 16, 21, 25],
-                 [9,  11, 12, 14, 16, 20, 24, 28],
-                 [11, 12, 14, 16, 19, 23, 26, 43],
-                 [12, 14, 16, 18, 22, 22, 21, 62],
-                 [14, 16, 18, 21, 23, 21, 42, 77],
-                 [16, 19, 22, 25, 21, 38, 76, 86],
-                 [20, 23, 25, 37, 38, 77, 82, 95],
-                 [24, 28, 46, 62, 75, 88, 98, 101]])
+ENTROPY_ZIGZAG_INVERSE = \
+    numpy.array([[0,  1,  5,  6,  14, 15, 27, 28],
+                 [2,  4,  7,  13, 16, 26, 29, 42],
+                 [3,  8,  12, 17, 25, 30, 41, 43],
+                 [9,  11, 18, 24, 31, 40, 44, 53],
+                 [10, 19, 23, 32, 39, 45, 52, 54],
+                 [20, 22, 33, 38, 46, 51, 55, 60],
+                 [21, 34, 37, 47, 50, 56, 59, 61],
+                 [35, 36, 48, 49, 57, 58, 62, 63]]).flatten()
+
+ENTROPY_ZIGZAG = numpy.argsort(ENTROPY_ZIGZAG_INVERSE)
 
 def RGB_YCOCG(pixels):
     m = [[1/4, 1/2, -1/4],
@@ -47,13 +48,44 @@ def YCOCG_RGB(pixels):
     rgb = numpy.clip(rgb, 0, 255)
     return numpy.rint(rgb).astype(numpy.uint8)
 
+def rle_encode(array):
+    t = array[0]
+    c = 0
+
+    out = []
+    for i in range(len(array)):
+        if array[i] != t:
+            out.extend([c, t])
+            t = array[i]
+            c = 0
+
+        c += 1
+
+    out.extend([c, t])
+
+    return numpy.asarray(out)
+
+def rle_decode(array):
+    if len(array) %2 != 0:
+        raise Exception("Not RLE!")
+
+    out = []
+    for i in range(math.floor(len(array)/2)):
+        idx = i*2
+        cnt = array[idx]
+        val = array[idx+1]
+
+        out.extend([val]*cnt)
+
+    return numpy.asarray(out)
+
 def resize_channel(pixels, w, h):
     return cv2.resize(pixels, (h, w), interpolation=cv2.INTER_AREA) 
 
 class DCTCompression:
     def __init__(self, tile_size=8, dct_quant=None):
         self.dct_quant = dct_quant if not dct_quant is None else DCT_QUANT_DEFAULT
-        self.tile_size = 8
+        self.tile_size = tile_size
 
     def compress_channels(self, f, channels):
         quant_data = self.dct_quant.astype(numpy.int32).tobytes()
@@ -62,14 +94,14 @@ class DCTCompression:
         f.write(struct.pack("HHI", self.tile_size, len(quant_data), len(channels)))
         f.write(quant_data)
 
-        for channel in channels:
+        for c,channel in enumerate(channels):
             chan_w, chan_h = channel.shape
             f.write(struct.pack("II", chan_w, chan_h))
 
-            num_tiles_pos = f.tell()
-            f.write(struct.pack("I", 0))
+            compressor = zlib.compressobj()
+            zlib_header = b""
 
-            k = 0
+            num_tiles = 0
             for j in range(math.ceil(chan_h/tile_size)):
                 for i in range(math.ceil(chan_w/tile_size)):
                     tile = numpy.zeros((tile_size, tile_size))
@@ -81,15 +113,18 @@ class DCTCompression:
                     tile /= self.dct_quant
                     tile = numpy.around(tile, 0)
 
-                    compressed_tile = zlib.compress(tile.astype(numpy.int8).tobytes())
-                    f.write(struct.pack("H", len(compressed_tile)))
-                    f.write(compressed_tile)
+                    tile = tile.flatten()[ENTROPY_ZIGZAG]
 
-                    k += 1
+                    rle_tile = rle_encode(tile).astype(numpy.int8).tobytes()
 
-            f.seek(num_tiles_pos, 0)
-            f.write(struct.pack("I", k))
-            f.seek(0, 2)
+                    zlib_header += compressor.compress(struct.pack("H", len(rle_tile)))
+                    zlib_header += compressor.compress(rle_tile)
+                    num_tiles += 1
+
+            compressed_channel = zlib_header + compressor.flush()
+
+            f.write(struct.pack("II", num_tiles, len(compressed_channel)))
+            f.write(compressed_channel)
 
     @staticmethod
     def decompress_channels(f):
@@ -99,18 +134,20 @@ class DCTCompression:
 
         output_channels = []
         for c in range(num_channels):
-            chan_w, chan_h, num_tiles = struct.unpack("III", f.read(12))
-            print(c, chan_w, chan_h, num_tiles)
+            chan_w, chan_h, num_tiles, compressed_channel_length = struct.unpack("IIII", f.read(16))
 
             output_channel = numpy.zeros((chan_w + tile_size, chan_h + tile_size))
+
+            channel = BytesIO(zlib.decompress(f.read(compressed_channel_length)))
 
             i = 0
             j = 0
             for t in range(num_tiles):
-                tile_length, = struct.unpack("H", f.read(2))
+                tile_length, = struct.unpack("H", channel.read(2))
 
-                compressed_tile = f.read(tile_length)
-                tile = numpy.frombuffer(zlib.decompress(compressed_tile), numpy.int8).reshape(tile_size, tile_size)
+                compressed_tile = channel.read(tile_length)
+                tile = rle_decode(numpy.frombuffer(compressed_tile, numpy.int8))
+                tile = tile[ENTROPY_ZIGZAG_INVERSE].reshape(tile_size, tile_size)
                 
                 tile = tile*dct_quant
                 tile = idct(idct(tile.T, norm='ortho').T, norm='ortho')
@@ -129,13 +166,13 @@ class DCTCompression:
 class ImageCompression:
     def __init__(self, chroma_scale, dct_quant):
         self.chroma_scale = chroma_scale
-        self.dct = DCTCompression(dct_quant)
+        self.dct = DCTCompression(dct_quant=dct_quant)
 
     @staticmethod
     def decompress(f):
         f.seek(0)
 
-        if f.read(FILE_SIG_L) != FILE_SIG:
+        if f.read(len(FILE_SIG)) != FILE_SIG:
             raise Exception("Signature mismatch!")
 
         img_w, img_h = struct.unpack("II", f.read(8))
@@ -148,10 +185,7 @@ class ImageCompression:
         u = resize_channel(u, img_h, img_w)
         v = resize_channel(v, img_h, img_w)
 
-        print(y.shape, u.shape, v.shape)
-
         yuv = numpy.dstack((y, u, v))
-        print(yuv.shape)
         pixels = YCOCG_RGB(yuv).astype(numpy.uint8)
 
         return Image.fromarray(pixels)
@@ -212,24 +246,44 @@ class MImg:
     def to_file(self, filename):
         self.save(open(filename, "wb"))
 
+def format_size(num, suffix="B"):
+    for unit in ("", "Ki", "Mi", "Gi", "Ti", "Pi", "Ei", "Zi"):
+        if abs(num) < 1024.0:
+            return f"{num:3.1f}{unit}{suffix}"
+        num /= 1024.0
+    return f"{num:.1f}Yi{suffix}"
+
 def test_compress_images():
     input_dir = "test-images"
     output_dir = "test-images-output"
 
     if not os.path.exists(output_dir):
         os.mkdir(output_dir)
+
+    print("testing compression in {0}".format(input_dir))
     
     for file in os.listdir(input_dir):
-        print(file)
         name,ext = os.path.splitext(file)
 
         output_filename = os.path.join(output_dir, name+".buf")
 
-        img = MImg.from_image(os.path.join(input_dir, file))
-        img.to_file(output_filename)
+        png_size = os.stat(os.path.join(input_dir, file)).st_size
 
-        img2 = MImg.from_file(output_filename)
-        img2._image.save(os.path.join(output_dir, file))
+        try:
+            t = time.time()
+            img = MImg.from_image(os.path.join(input_dir, file))
+            img.to_file(output_filename)
+            compressed_size = os.stat(output_filename).st_size
+            t1 = time.time()
+
+            img2 = MImg.from_file(output_filename)
+            img2._image.save(os.path.join(output_dir, file))
+            t2 = time.time()
+
+            print("file={0} compression={1:.1f}s decompression={2:.1f}s png={3} compressed={4} ratio={5:.2%}".format(file, t1-t, t2-t1, format_size(png_size), format_size(compressed_size), 1-(compressed_size/png_size)))
+        except Exception as e:
+            print("error compressing {0}: {1}".format(file, e))
 
 if __name__ == "__main__":
+    import time
     test_compress_images()
---
+++ test-pygame.py
@@ -0,0 +1,63 @@
+import pygame
+import os
+import time
+from PIL import Image
+from img import MImg
+
+img_dir = "test-images-output"
+img_ext = ".buf"
+
+pygame.init()
+
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
+
+font = pygame.font.SysFont("Calibri", 20, True)
+
+images = []
+for f in os.listdir(img_dir):
+    if os.path.splitext(f)[1] == img_ext:
+        images.append(os.path.join(img_dir, f))
+
+idx = 0
+
+running = True
+load = True
+img_surf = None
+while running:
+    if load:
+        t = time.time()
+        img = MImg.from_file(images[idx])._image
+        t2 = time.time()
+        img_surf = pygame.image.fromstring(img.tobytes(), img.size, img.mode).convert()
+        t3 = time.time()
+        load = False
+
+        print("loading={0:.02f}s pygame={1:.02f}s total={2:.02f}".format(t2-t, t3-t2, t3-t))
+
+    screen.fill((255, 255, 255))
+
+    screen.blit(img_surf, (10, 10))
+    screen.blit(font.render("{0:.0f}".format(clock.get_fps()), True, (0, 0, 0)), (0, 0))
+
+    pygame.display.flip()
+
+    for e in pygame.event.get():
+        if e.type == pygame.QUIT:
+            pygame.quit()
+            running = False
+
+        if e.type == pygame.KEYDOWN:
+            if e.key == pygame.K_RIGHT:
+                idx += 1
+                load = True
+            if e.key == pygame.K_LEFT:
+                idx -= 1
+                load = True
+            
+            if idx <= 0:
+                idx = len(images)-1
+            if idx >= len(images):
+                idx = 0
+    
+    clock.tick(60)
